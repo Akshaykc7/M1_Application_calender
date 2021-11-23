c957a237dfad76ae6f0bf098c8e11d29d4b37923
Akshay K C <92025005+Akshaykc7@users.noreply.github.com>
2021-11-23 21:12:34 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,1 +1,18 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-07-11
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+#include"calc.h"
+
+/**
+ * @brief main function of the project
+ * 
+ * @return int 
+ */
